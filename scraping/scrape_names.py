e86e77254afa8c097afbd86095e86e85c12cfcb3
Karl Beckman <karlbeckman97@gmail.com>
2021-04-29 18:44:09 +0200

    created a function that scrapes name day names and returns them in a list

---
+++ scraping/scrape_names.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+
+def namsdagsNamn(className):
+    # Read url and parse data
+    webUrl = urllib.request.urlopen('https://www.kalender.se/')
+    data = webUrl.read()
+    soup = BeautifulSoup(data, 'html.parser')
+
+    # Extract correct content from the html, where the names are
+    today = soup.find("table", { "class" : "monthback" }).find("td", {"class": className})
+    nameContainer = today.find("div", {"class": "V10"})
+    nameContainer = str(nameContainer)
+    
+    # Extract the names into a list
+    pattern = re.compile('>{1}(\w+,?\s*\w*)<br/>{1}')
+    names = pattern.findall(nameContainer)[0]
+    names = names.split(',')
+    names = [n.strip() for n in names]
+    
+    return names
+    
+def main():
+    className = "monthreg"
+    className = "today"
+    namnsdagsbarn = namsdagsNamn(className)
+    print(namnsdagsbarn)
+
+    
+if __name__ == "__main__":
+    main()
+
